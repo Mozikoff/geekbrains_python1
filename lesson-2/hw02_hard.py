1e49b95ebb99a9e47d280766c5c51896d0aee62e
emozikov <emozikov@ozon.ru>
2018-11-20 12:23:22 +0300

    hard task 1 solved

--- lesson-2/hw02_hard.py
+++ lesson-2/hw02_hard.py
@@ -5,6 +5,17 @@ import string
 # 1. Сколько слов в тексте?
 # 2. Сколько букв английского алфавита в тексте?
 
+
+def statistics(txt):
+    print('Your text:')
+    print(txt)
+    table = str.maketrans({key: None for key in string.punctuation})
+    txt = txt.translate(table)
+    word_count = len(txt.lower().split())
+    english_letters_count = len(list(filter(lambda char: char in string.ascii_letters, txt)))
+    print(f'Word count: {word_count}, english letters count: {english_letters_count}')
+
+
 # Задача-2 Пользователь вводит два текста, необходимо найти все слова, которые есть в обоих текстах. Без учета регистра
 
 
@@ -19,3 +30,6 @@ if __name__ == '__main__':
     txt1 = 'Some text about programming in Python!'
     txt2 = 'How about some coffee, Python?'
     print(intersect(txt1, txt2))
+    print('-------------------------------------------------------------')
+    txt = 'Текст для демонстрации возможностей метода statistics. Это чуть посложнее чем "Hello world!"'
+    statistics(txt)
